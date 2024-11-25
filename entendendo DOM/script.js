7a48db869eab981ece7c5ac614417bafe3a1fc73
Tamires Rodrigues Delmondes <89863897+tamiresrodrigue@users.noreply.github.com>
2024-11-25 20:59:25 -0300

    Add files via upload

---
+++ entendendo DOM/script.js
@@ -0,0 +1,30 @@
+const diaSemana =Number(prompt("Digite um dia da semana:1-7"))
+
+switch(diaSemana){
+    case 1:
+        alert("Domingo")
+        break;
+    case 2:
+        alert("Segunda")
+        break;
+    case 3:
+        alert("Terça")
+        break;
+    case 4:
+        alert("Quarta")
+        break;
+    case 5:
+        alert("Quinta")
+        break;
+    case 6:
+        alert("Sexta")
+        break;
+    case 7:
+        alert("Sábado")
+        break;
+    default:
+        alert("Não exite este dia da semana")
+        break;
+
+
+}
